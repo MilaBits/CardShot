5e913cd2ba32327e9ce861178580ec56b0e6d680
Milly <milabits@gmail.com>
2019-02-14 22:04:11 +0100

    Added Block Cards

---
+++ Assets/Scripts/Cards/CardEffects/Block.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card Effect (Block)", menuName = "Cards/Card Effects/Block")]
+[Serializable]
+public class Block : CardEffect {
+    [BoxGroup("$name")]
+    [LabelWidth(100), OnValueChanged("UpdateDescription")]
+    public int HitThreshold;
+
+    [BoxGroup("$name")] [LabelWidth(100), OnValueChanged("UpdateDescription")]
+    public float Duration;
+
+    private void UpdateDescription() {
+        string firstbit;
+        if (Target == EffectTarget.Area) {
+            firstbit = "Place wall that can";
+        }
+        else {
+            firstbit = "Personal shield that can";
+        }
+        
+        Description = $"{firstbit} block up to {HitThreshold} hits in the next {Duration} seconds";
+    }
+
+    public override void ExecuteArea(Vector3 position) {
+        Debug.Log($"{name} hit {position}");
+    }
+
+    public override void ExecutePlayer(Player player) {
+        Debug.Log($"{name} hit {player.name}");
+    }
+}
