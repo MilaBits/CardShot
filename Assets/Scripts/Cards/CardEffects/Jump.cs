998f3b8d63a2195f7274d7cdd9f4f8b5a308f4ee
Milly <milabits@gmail.com>
2019-03-29 15:46:01 +0100

    More cards, shields! need to fix player collider

---
+++ Assets/Scripts/Cards/CardEffects/Jump.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using Cards;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card Effect (Speed)", menuName = "Cards/Card Effects/Speed")]
+[Serializable]
+public class Speed : CardEffect {
+    [BoxGroup("$name")]
+    [LabelWidth(100), OnValueChanged("UpdateDescription"), Range(-0, 2)]
+    public float SpeedMultiplier;
+
+    [BoxGroup("$name")]
+    [LabelWidth(100), OnValueChanged("UpdateDescription")]
+    public float Duration;
+
+    [BoxGroup("$name")]
+    public Modifier TargetModifier;
+
+    private void Awake() {
+        UpdateDescription();
+    }
+
+    private void UpdateDescription() {
+        string multiplier = string.Empty;
+        multiplier = SpeedMultiplier > 1 ? "Increase" : "Decrease";
+
+        Description =
+            $"{multiplier} Target's movement speed by {Math.Abs(SpeedMultiplier * 100 - 100)}% for {Duration} seconds";
+    }
+
+    public override void ExecuteArea(UseInfo info) {
+        Debug.Log($"{name} hit {info.TargetPosition}");
+    }
+
+    public override void ExecutePlayer(PlayerModifiers playerModifiers) {
+        Debug.Log("Executing on: " + playerModifiers.gameObject.name);
+
+        // Check if the recipient can recive the modifier
+        Modifier modifier = playerModifiers.Modifiers.OfType<SpeedModifier>().First();
+        if (modifier != null && modifier.isModifying && !modifier.isStackable) return;
+
+        modifier.Modify(playerModifiers, SpeedMultiplier, Duration);
+
+        // Decide if it should be the positive or negative sprite
+        if (SpeedMultiplier > 1) {
+            playerModifiers.AddStatUI(modifier.PositiveImage, Duration, modifier.PositiveColor);
+        }
+        else {
+            playerModifiers.AddStatUI(modifier.NegativeImage, Duration, modifier.NegativeColor);
+        }
+    }
+}
---
+++ Assets/Scripts/Cards/Modifiers/JumpModifier.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Modifier (Speed)", menuName = "Cards/Modifiers/Speed")]
+public class SpeedModifier : Modifier
+{
+    [BoxGroup("$name")]
+    [SerializeField]
+    private PlayerCharacterController Controller;
+
+    private float OriginalSpeed;
+
+    [BoxGroup("$name")]
+    [SerializeField, ReadOnly]
+    private float Duration;
+
+    private IEnumerator coroutine;
+
+    public override void Modify(PlayerModifiers modifiers, float value, float duration)
+    {
+        Controller = modifiers.GetComponent<PlayerCharacterController>();
+
+        OriginalSpeed = Controller.speed;
+        Controller.speed *= value;
+        isModifying = true;
+
+        coroutine = LastDuration(duration);
+        // This feels hacky?????
+        Controller.StartCoroutine(coroutine);
+
+        Debug.Log(modifiers.gameObject.name + " hit by " + name);
+    }
+
+    public override void Cancel()
+    {
+        Controller.StopCoroutine(coroutine);
+        ResetModifier();
+    }
+
+    private void ResetModifier()
+    {
+        Controller.speed = OriginalSpeed;
+        isModifying = false;
+    }
+
+    IEnumerator LastDuration(float duration)
+    {
+        Duration = duration;
+        yield return new WaitForSeconds(duration);
+        ResetModifier();
+    }
+}
